0ca3f8b2979fabb348dc56e36c2def4cd9e4ee8a
harsh <hs669972@gmail.com>
2024-04-07 19:11:15 +0530

    some changes

--- test.py
+++ test.py
@@ -1,19 +1,18 @@
 import sqlite3
 
-# Connect to the database
+
 conn = sqlite3.connect('users.db')
 cursor = conn.cursor()
 
-# Fetch table names
+
 cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
 tables = cursor.fetchall()
 
-# Print table names
 print("Tables:")
 for table in tables:
     print(table[0])
 
-# Fetch and print data from each table
+
 for table in tables:
     print("\nData in table:", table[0])
     cursor.execute("SELECT * FROM " + table[0])
@@ -21,5 +20,5 @@ for table in tables:
     for row in data:
         print(row)
 
-# Close the connection
+
 conn.close()
